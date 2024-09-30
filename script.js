c450f770fce3df81ec56da7cf16f46e7f155df54
Fauzi-kun <fauzimalik08@gmail.com>
2024-09-30 19:50:24 +0700

    new feat

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-const inputBox = document.getElementById("input-box");
-const listBody = document.getElementById("list-body");
-const priority = document.getElementById("priority");
-const addList = document.getElementById("add");
+const time = new Date()
+let date = time.toLocaleString()
 
-addList.addEventListener('click', function(){
-    if(inputBox.value === ''){
-        alert("mohon tulis list");
-    }
-    else{
-        let list = document.createElement("li");
-        list.innerHTML = inputBox.value + "-" + priority.value;
-        listBody.appendChild(list);
-        inputBox.value = '';
-        saveData();
+
+
+
+const profil = document.getElementById('profil')
+
+
+document.getElementById('btn-name').addEventListener('click', function(){
+    let nama = document.getElementById('name').value.trim()
+    let pos = document.getElementById('pos').value.trim()
+    if(nama !=='' && pos !== ''){
+    profil.innerHTML = `Nama : ${nama} - Possition : ${pos}`
     }
 })
-function saveData(){
-    localStorage.setItem("data", listBody.innerHTML);
-}
-function showTask(){
-    listBody.innerHTML = localStorage.getItem("data");
+
+
+
+document.getElementById('add').addEventListener('click', function(){
+    let inputBox = document.getElementById('input-box')
+    let priority = document.getElementById('priority')
+    let listTask = document.getElementById('list-task')
+    if(inputBox.value !== ''){
+        let add = document.createElement('li')
+        add.classList.add(`priority-${priority.value}`)
+        add.innerHTML = `<input type="checkbox" class="checkbox"><span class='taskText'>${inputBox.value.trim()} - ${priority.value} - ${date}</span><button class='removeBtn'>Remove</button>`
+        listTask.appendChild(add)
+    }
+    inputBox.value = ''
+
+    listTask.querySelector('.checkbox').addEventListener('change', function(){  
+        if(this.checked){
+            toDone(add)
+        }
+    })
+
+})
+function toDone(item){
+    const doneTask = document.getElementById('done-task')
+    const newItem =item
+        newItem.classList.add('complete')
+        
+        doneTask.appendChild(newItem)
 }
